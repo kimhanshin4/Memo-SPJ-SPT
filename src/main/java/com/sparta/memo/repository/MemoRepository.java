4fd2b281aeedb3adc6e14c727c398d1f4305aba9
kimhanshin4 <funold1004@gmail.com>
2023-11-09 11:49:19 +0900

    Memo - Spring Data JPA 실습!

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -1,90 +1,89 @@
 package com.sparta.memo.repository;
 
-import com.sparta.memo.dto.*;
 import com.sparta.memo.entity.*;
-import jakarta.persistence.*;
-import java.sql.*;
-import java.util.*;
-import org.springframework.jdbc.core.*;
-import org.springframework.jdbc.support.*;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.*;
-import org.springframework.transaction.annotation.*;
 
-@Repository
-public class MemoRepository {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    public MemoRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate; //생성자로 DI - 객체의 불변성을 지켜줄 수 있음
-    }
-
-    public Memo save(Memo memo) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
-        jdbcTemplate.update(con -> {
-                PreparedStatement preparedStatement = con.prepareStatement(sql,
-                    Statement.RETURN_GENERATED_KEYS);
-
-                preparedStatement.setString(1, memo.getUsername());
-                preparedStatement.setString(2, memo.getContents());
-                return preparedStatement;
-            },
-            keyHolder);
-
-        Long id = keyHolder.getKey().longValue();
-        memo.setId(id);
-        return memo;
-    }
+//@Repository // SimpleJpaRepository에 @Repository가 달려있다.
+public interface MemoRepository extends JpaRepository<Memo, Long> {
 
 
-    public void update(Long id, MemoRequestDto requestDto) {
-        String sql = "UPDATE memo SET username = ?, contents = ? WHERE id = ?";
-        jdbcTemplate.update(sql, requestDto.getUsername(), requestDto.getContents(), id);
-    }
-
-    public void delete(Long id) {
-        String sql = "DELETE FROM memo WHERE id = ?";
-        jdbcTemplate.update(sql, id);
-    }
-
-    public Memo findById(Long id) {
-        String sql = "SELECT * FROM memo WHERE id = ?";
-
-        return jdbcTemplate.query(sql, resultSet -> {
-            if (resultSet.next()) {
-                Memo memo = new Memo();
-                memo.setUsername(resultSet.getString("username"));
-                memo.setContents(resultSet.getString("contents"));
-                return memo;
-            } else {
-                return null;
-            }
-        }, id);
-    }
-
-    public List<MemoResponseDto> findAll() {
-        String sql = "SELECT * From memo";
-
-        return jdbcTemplate.query(sql, new RowMapper<MemoResponseDto>() {
-            @Override
-            public MemoResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Long id = rs.getLong("id");
-                String username = rs.getString("username");
-                String contents = rs.getString("contents");
-                return new MemoResponseDto(id, username, contents);
-            }
-        });
-    }
-
-    @Transactional
-    public Memo createMemo(EntityManager em) {
-        Memo memo = em.find(Memo.class, 1);
-        memo.setUsername("Robbert");
-        memo.setContents("@Transactional 전파 테스트 중!");
-
-        System.out.println("createMemo 메서드 종료");
-        return memo;
-    }
 }
+
+//JDBC Template 잘가...
+//public class MemoRepository {
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    public MemoRepository(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate; //생성자로 DI - 객체의 불변성을 지켜줄 수 있음
+//    }
+//
+//    public Memo save(Memo memo) {
+//        KeyHolder keyHolder = new GeneratedKeyHolder();
+//
+//        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
+//        jdbcTemplate.update(con -> {
+//                PreparedStatement preparedStatement = con.prepareStatement(sql,
+//                    Statement.RETURN_GENERATED_KEYS);
+//
+//                preparedStatement.setString(1, memo.getUsername());
+//                preparedStatement.setString(2, memo.getContents());
+//                return preparedStatement;
+//            },
+//            keyHolder);
+//
+//        Long id = keyHolder.getKey().longValue();
+//        memo.setId(id);
+//        return memo;
+//    }
+//
+//
+//    public void update(Long id, MemoRequestDto requestDto) {
+//        String sql = "UPDATE memo SET username = ?, contents = ? WHERE id = ?";
+//        jdbcTemplate.update(sql, requestDto.getUsername(), requestDto.getContents(), id);
+//    }
+//
+//    public void delete(Long id) {
+//        String sql = "DELETE FROM memo WHERE id = ?";
+//        jdbcTemplate.update(sql, id);
+//    }
+//
+//    public Memo findById(Long id) {
+//        String sql = "SELECT * FROM memo WHERE id = ?";
+//
+//        return jdbcTemplate.query(sql, resultSet -> {
+//            if (resultSet.next()) {
+//                Memo memo = new Memo();
+//                memo.setUsername(resultSet.getString("username"));
+//                memo.setContents(resultSet.getString("contents"));
+//                return memo;
+//            } else {
+//                return null;
+//            }
+//        }, id);
+//    }
+//
+//    public List<MemoResponseDto> findAll() {
+//        String sql = "SELECT * From memo";
+//
+//        return jdbcTemplate.query(sql, new RowMapper<MemoResponseDto>() {
+//            @Override
+//            public MemoResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                Long id = rs.getLong("id");
+//                String username = rs.getString("username");
+//                String contents = rs.getString("contents");
+//                return new MemoResponseDto(id, username, contents);
+//            }
+//        });
+//    }
+//
+//    @Transactional
+//    public Memo createMemo(EntityManager em) {
+//        Memo memo = em.find(Memo.class, 1);
+//        memo.setUsername("Robbert");
+//        memo.setContents("@Transactional 전파 테스트 중!");
+//
+//        System.out.println("createMemo 메서드 종료");
+//        return memo;
+//    }
+//}
--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,39 +4,18 @@ import com.sparta.memo.dto.*;
 import com.sparta.memo.entity.*;
 import com.sparta.memo.repository.*;
 import java.util.*;
-import org.springframework.context.*;
 import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
 
 
-//@Component // 넌 이제부터 bean이다
-//@RequiredArgsConstructor //final이 달린(상수) 필드를 가지는 '생성자'를 만들어줌 // Lombok으로 DI
-@Service //세상 명확한 ANtation
-public class MemoService { //memoService 로 bean에 등록
+@Service
+public class MemoService {
 
-    //    @Autowired //편하고 용이하지만 불안정성 때문에 현업에서도 대부분 생성자 DI를 사용
-//    private MemoRepository memoRepository; //만들어진 Repository
     private final MemoRepository memoRepository;
 
-//    public void setDi(MemoRepository memoRepository) { //메서드로 DI
-//        this.memoRepository = memoRepository;
-//    }
-
-    //    final - 초기화 필요 => 아래 처럼 생성자로 초기화
-    public MemoService(MemoRepository memoRepository) { //생성자로 DI
-        this.memoRepository = memoRepository; //를 사용!
+    public MemoService(MemoRepository memoRepository) {
+        this.memoRepository = memoRepository;
     }
-
-    //수동으로 DI
-//    public MemoService(ApplicationContext context) {
-//    1. 'Bean'이름으로 가져오기 //Component한 클래스를 직접 IoC Container에 접근해서 꺼내오기
-//        MemoRepository memoRepository = (MemoRepository) context.getBean(
-//            "memoRepository");//(클래스를 캐스팅) 후 Bean의 이름으로 가져옴
-//        this.memoRepository = memoRepository;
-//    }
-//    2. 'Bean'클래스 형식으로 가져오기
-//        MemoRepository memoRepository = context.getBean(MemoRepository.class);
-//        this.memoRepository = memoRepository;
-//    }
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         Memo memo = new Memo(requestDto);
@@ -47,30 +26,36 @@ public class MemoService { //memoService 로 bean에 등록
     }
 
     public List<MemoResponseDto> getMemos() {
-        return memoRepository.findAll();
+        //DB 조회
+        return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
+//        return memoRepository.findAll();
     }
 
+    @Transactional
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
-        Memo memo = memoRepository.findById(id);
-        if (memo != null) {
-            // memo 내용 수정
-            memoRepository.update(id, requestDto);
+        //해당 메모가 DB에 존재하는지 확인
+        Memo memo = findMemo(id);
+        memo.update(requestDto);
 
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        return id;
+
     }
 
 
     public Long deleteMemo(Long id) {
-        Memo memo = memoRepository.findById(id);
-        if (memo != null) {
-            memoRepository.delete(id);
+        //해당 메모가 DB에 존재하는지 확인
+        Memo memo = findMemo(id);
 
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        //memo 삭제
+        memoRepository.delete(memo);
+        return id;
+
+    }
+
+    //중복 코드 메서드화
+    private Memo findMemo(Long id) {
+        return memoRepository.findById(id).orElseThrow(() ->
+            new IllegalArgumentException("선택한 메모는 존재하지 않는다.")
+        );
     }
 }
