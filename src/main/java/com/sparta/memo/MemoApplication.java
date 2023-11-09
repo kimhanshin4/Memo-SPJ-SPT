8bc2f365b93529e00e3ec6ac4ff11ba147d0b504
kimhanshin4 <funold1004@gmail.com>
2023-11-09 12:12:48 +0900

    Memo - JPA Auditing 적용하기 (Timestamped)

--- src/main/java/com/sparta/memo/MemoApplication.java
+++ src/main/java/com/sparta/memo/MemoApplication.java
@@ -2,7 +2,9 @@ package com.sparta.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.*;
 
+@EnableJpaAuditing
 @SpringBootApplication // 스프링 부트로서의 역할 (@ComponentScan 등등...)
 public class MemoApplication {
 
--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -1,6 +1,7 @@
 package com.sparta.memo.dto;
 
 import com.sparta.memo.entity.Memo;
+import java.time.*;
 import lombok.Getter;
 
 @Getter
@@ -9,16 +10,14 @@ public class MemoResponseDto {
     private Long id;
     private String username;
     private String contents;
+    private LocalDateTime creatAt;
+    private LocalDateTime modifiedAt;
 
     public MemoResponseDto(Memo memo) {
         this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
-    }
-
-    public MemoResponseDto(Long id, String username, String contents) {
-        this.id = id;
-        this.username = username;
-        this.contents = contents;
+        this.creatAt = memo.getCreatedAt();
+        this.modifiedAt = memo.getModifiedAt();
     }
 }
